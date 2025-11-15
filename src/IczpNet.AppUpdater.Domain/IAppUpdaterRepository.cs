142f4b0cebd3d0bb456cfcdb872c0c5f8b016930
zhongpei <1000@intry.cn>
2025-11-15 10:05:33 +0800

    0

---
+++ src/IczpNet.AppUpdater.Domain/IAppUpdaterRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Repositories;
+
+namespace IczpNet.AppUpdater;
+
+public interface IAppUpdaterRepository<TEntity, TKey> : IRepository<TEntity, TKey>, IAppUpdaterRepository
+    where TEntity : class, IEntity<TKey>
+{
+    /// <summary>
+    /// 获取多个
+    /// </summary>
+    /// <param name="idList"></param>
+    /// <returns></returns>
+    Task<IEnumerable<TEntity>> GetManyAsync(IEnumerable<TKey> idList);
+
+    /// <summary>
+    /// 获取多个
+    /// </summary>
+    /// <param name="idList"></param>
+    /// <returns></returns>
+    Task<IEnumerable<TEntity>> FindManyAsync(IEnumerable<TKey> idList);
+}
+
+public interface IAppUpdaterRepository : IRepository
+{
+}
---
+++ src/IczpNet.AppUpdater.EntityFrameworkCore/AppUpdaterRepository.cs
@@ -0,0 +1,32 @@
+﻿using IczpNet.AppUpdater.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.Timing;
+
+namespace IczpNet.AppUpdater;
+
+public abstract class AppUpdaterRepository<TEntity, TPrimaryKey>(IDbContextProvider<AppUpdaterDbContext> dbContextProvider) : EfCoreRepository<AppUpdaterDbContext, TEntity, TPrimaryKey>(dbContextProvider), IAppUpdaterRepository<TEntity, TPrimaryKey>
+        where TEntity : class, IEntity<TPrimaryKey>
+{
+    protected IClock Clock => LazyServiceProvider.LazyGetRequiredService<IClock>();
+
+    public async Task<IEnumerable<TEntity>> FindManyAsync(IEnumerable<TPrimaryKey> idList)
+    {
+        return await GetListAsync(x => idList.Contains(x.Id));
+    }
+
+    public async Task<IEnumerable<TEntity>> GetManyAsync(IEnumerable<TPrimaryKey> idList)
+    {
+        var list = new List<TEntity>();
+        foreach (var id in idList)
+        {
+            var entity = await GetAsync(id);
+            list.Add(entity);
+        }
+        return list;
+    }
+}
