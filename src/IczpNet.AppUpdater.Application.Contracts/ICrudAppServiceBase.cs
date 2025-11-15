a27d74372558b612e53f003a74b9f680a0fbc5e6
zhongpei <1000@intry.cn>
2025-11-15 09:21:53 +0800

    0

---
+++ src/IczpNet.AppUpdater.Application.Contracts/ICrudAppServiceBase.cs
@@ -0,0 +1,10 @@
+﻿using IczpNet.AbpCommons;
+using System.Threading.Tasks;
+
+namespace IczpNet.AppUpdater;
+
+public interface ICrudAppServiceBase<TGetOutputDto, TGetListOutputDto, TKey, TGetListInput, TCreateInput, TUpdateInput> : ICrudAbpCommonsAppService<TGetOutputDto, TGetListOutputDto, TKey, TGetListInput, TCreateInput, TUpdateInput>
+{
+
+    Task<bool> SetIsEnabledAsync(TKey id, bool isEnabled);
+}
