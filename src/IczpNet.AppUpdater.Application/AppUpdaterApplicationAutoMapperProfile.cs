3fc2aca525562f19ff0504d76fd065dc2179f470
zhongpei <1000@intry.cn>
2025-11-15 09:22:10 +0800

    0

--- src/IczpNet.AppUpdater.Application/AppUpdaterApplicationAutoMapperProfile.cs
+++ src/IczpNet.AppUpdater.Application/AppUpdaterApplicationAutoMapperProfile.cs
@@ -9,5 +9,7 @@ public class AppUpdaterApplicationAutoMapperProfile : Profile
         /* You can configure your AutoMapper mapping configuration here.
          * Alternatively, you can split your mapping configurations
          * into multiple profile classes for a better organization. */
+
+        /*---------code-generator-mapper: Do not modify or delete this line of comments--------*/
     }
 }
